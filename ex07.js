2d8d192f202efa25e8b0b699bd3a0beb21ee3d73
KennyAle <kennyviver@gmail.com>
2024-09-23 09:27:59 -0700

    Exercise 07

--- ex07.js
+++ ex07.js
@@ -1,7 +1,12 @@
 // Exercise 7: Write a function named 'calculateAverage' that takes an array of numbers and returns the average value.
 
+let sum = 0
 function calculateAverage(arr) {
-  // your code here
+  for (let index = 0; index < arr.length; index++) {
+    sum += arr[index]
+  }
+  let average = sum / arr.length
+  return average
 }
 
 console.log(calculateAverage([10, 20, 30, 40, 50])) // 30
