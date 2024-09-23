bb658f16129ec58bfd300a93a200ed2a22932634
KennyAle <kennyviver@gmail.com>
2024-09-23 11:30:29 -0700

    Exercise 12

--- ex12.js
+++ ex12.js
@@ -1,7 +1,12 @@
 // Exercise 12: Write a function named 'filterAdults' that takes an array of user objects (each with an 'age' property) and returns a new array with only the users who are 18 or older.
-
+let ofAge = []
 function filterAdults(users) {
-  // your code here
+  for (e in users) {
+    if (users[e].age >= 18) {
+      ofAge.push(users[e])
+    }
+  }
+  return ofAge
 }
 
 console.log(filterAdults([{ name: 'Jane', age: 18 }, { name: 'Joe', age: 19 }, { name: 'Jim', age: 17 }])) // [{ name: 'Jane', age: 18 }, { name: 'Joe', age: 19 }]
