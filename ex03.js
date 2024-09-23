eb25ade4c935afd78ebbfd6da27395e3f358f5b6
KennyAle <kennyviver@gmail.com>
2024-09-23 09:51:19 -0700

    Exercise 03

--- ex03.js
+++ ex03.js
@@ -1,7 +1,13 @@
 // Exercise 3: Write a function named 'getFullNames' that takes an array of user objects (each with 'firstName' and 'lastName' properties) and returns an array of full names (e.g., 'John Doe'). 
 
+let fullNames = []
+
 function getFullNames(users) {
-  // your code here
+  for (let index = 0; index < users.length; index++) {
+    const element = users[index].firstName + ' ' + users[index].lastName
+    fullNames.push(element)
+  }
+  return fullNames
 }
 
 console.log(getFullNames([{ firstName: 'John', lastName: 'Doe' }, { firstName: 'Jane', lastName: 'Doe' }])) // ['John Doe', 'Jane Doe']
